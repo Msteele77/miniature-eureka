18cec185b098c1effe15e1cb29c42beb9c6bd52b
Macheya Steele <mcheysteele@gmail.com>
2021-02-09 18:07:45 -0700

    server logic

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
 
+const routes = require('./routes/routes');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-require('./routes/routes')(app);
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
+
+
+
