7e408abed9d991a8337b2baa75af381f27b1efd6
Macheya Steele <mcheysteele@gmail.com>
2021-04-18 11:39:33 -0600

    updated to routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
 module.exports = function (app) {
 
  
 
-  app.get("api/notes", function (req, res) {
+  app.get("api/notes", (req, res) => {
 
     res.json(data);
 
@@ -14,32 +16,18 @@ module.exports = function (app) {
     res.json(data[Number(req.params.id)]);
   });
 
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
-
-
-  app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
-
-
-
-
-
-  app.post("/notes", (req, res) => {
+  app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     let uniqueId = (data.length).toString();
     console.log(uniqueId);
     newNote.id = uniqueId;
     data.push(newNote);
     //console.log(newNote);
-    fs.writeFileSync("db/db.json", JSON.stringify(notes));
-    if (err) throw (err);
-  
-    res.json(notes);
-  });
-  
-  //delete notes
+    fs.writeFileSync("db/db.json", JSON.stringify(data),  function(err) {
+      if (err) throw (err);        
+  }); 
+
+  res.json(data);    
+
+})
 }
