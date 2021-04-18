021163fcbcadc2de9d0ae5d4f0d3a205d97b8e3c
Macheya Steele <mcheysteele@gmail.com>
2021-04-18 11:55:03 -0600

    updated sever.js and route

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-require("./routes/html-routes")(app);
-require("./routes/routes")(app);
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use("/assets", express.static("./assets"));
 
+require("./routes/html-routes")(app);
+require("./routes/routes")(app);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
